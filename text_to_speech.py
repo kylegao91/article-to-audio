20033665003995223d1e5b3679394da6a887b2dd
Kyle Gao <kgao@abnormalsecurity.com>
2023-02-28 18:21:08 -0800

    use azure instead of aws for tts

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,49 +1,46 @@
-"""Getting Started Example for Python 2.7+/3.3+"""
-import logging
-from contextlib import closing
-
-from boto3 import Session
-from botocore.exceptions import BotoCoreError, ClientError
+import os
+import sys
+import azure.cognitiveservices.speech as speechsdk
 
 
-class TextToSpeech(object):
+class TextToSpeech:
+    def __init__(self):
+        # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+        self.speech_config = speechsdk.SpeechConfig(
+            subscription=os.environ.get("SPEECH_KEY"),
+            region=os.environ.get("SPEECH_REGION"),
+        )
 
-  def __init__(self, profile="default"):
-    # Create a client using the credentials and region defined in the [adminuser]
-    # section of the AWS credentials file (~/.aws/credentials).
-    session = Session(profile_name=profile)
-    self.polly = session.client("polly")
+        # The language of the voice that speaks.
+        self.speech_config.speech_synthesis_voice_name = "en-US-JennyNeural"
 
-  def convert(self, text, output):
-    logging.info("Converting text to speech")
-    try:
-        # Request speech synthesis
-        response = self.polly.synthesize_speech(Text=text, OutputFormat="mp3", VoiceId="Joanna")
-    except (BotoCoreError, ClientError) as error:
-        # The service returned an error, exit gracefully
-        print(error)
-        return False
+    def convert(self, text, output_file):
+        audio_config = speechsdk.audio.AudioOutputConfig(filename=output_file)
+        speech_synthesizer = speechsdk.SpeechSynthesizer(
+            speech_config=self.speech_config, audio_config=audio_config
+        )
+        speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
 
-    # Access the audio stream from the response
-    if "AudioStream" in response:
-        # Note: Closing the stream is important because the service throttles on the
-        # number of parallel connections. Here we are using contextlib.closing to
-        # ensure the close method of the stream object will be called automatically
-        # at the end of the with statement's scope.
-        with closing(response["AudioStream"]) as stream:
-          print("Writing speech to file: " + output)
-          try:
-              # Open a file for writing the output as a binary stream
-              with open(output, "wb") as file:
-                file.write(stream.read())
-          except IOError as error:
-              # Could not write to file, exit gracefully
-              print(error)
-              return False
+        if (
+            speech_synthesis_result.reason
+            == speechsdk.ResultReason.SynthesizingAudioCompleted
+        ):
+            print("Speech synthesized for text [{}]".format(text))
+        elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = speech_synthesis_result.cancellation_details
+            print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+            if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                if cancellation_details.error_details:
+                    print(
+                        "Error details: {}".format(cancellation_details.error_details)
+                    )
+                    print("Did you set the speech resource key and region values?")
 
-    else:
-        # The response didn't contain audio data, exit gracefully
-        print("Could not stream audio")
-        return False
 
-    return True
+if __name__ == "__main__":
+    client = TextToSpeech()
+
+    text = sys.argv[1]
+    output_path = sys.argv[2]
+
+    client.convert(text, output_path)
